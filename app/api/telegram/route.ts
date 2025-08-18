3d699d3f102620e268daaf84f6561eb927b9d484
Amir Ali Hemmati <amir.negative21@gmail.com>
2025-08-18 13:48:54 +0330

    Update

--- app/api/telegram/route.ts
+++ app/api/telegram/route.ts
@@ -4,14 +4,12 @@ import UserService from "./service";
 export async function POST(req: Request) {
   const body = await req.json();
 
-  try {
-    await UserService.createUser(
-      body.message.from.id,
-      body.message.from.first_name,
-      body.message.from.last_name,
-      body.message.from.username,
-    );
-  } catch (e) {}
+  UserService.createUser(
+    body.message.from.id,
+    body.message.from.first_name,
+    body.message.from.last_name,
+    body.message.from.username,
+  ).catch(() => {});
 
   return NextResponse.json({
     method: "sendMessage",
